7bb8ed16dff8e0d153cb3011f45bc53ee6c04910
Bei <573321594@qq.com>
2024-01-23 23:34:30 -0800

    common component lifting

--- client-react/src/components/home/tabPanels/HRpages/VisaManagementPage.jsx
+++ client-react/src/components/home/tabPanels/HRpages/VisaManagementPage.jsx
@@ -1,12 +1,20 @@
 import React from "react";
-import PropTypes from "prop-types";
-import Typography from "@mui/material/Typography";
-import Box from "@mui/material/Box";
 
+import CommomTabsLayout from "./CommomTabsLayout";
+import InProcessPage from "./visaPages/InProcessPage";
+import AllEmployeeVisaPages from "./visaPages/AllEmployeeVisaPages";
 export default function VisaManagementPage() {
     return (
-        <div>
-            VisaManagementPage
-        </div>
+        <CommomTabsLayout
+            tabs={[
+                { label: "All" },
+                { label: "In Process" }
+            ]}
+            tabPanels={[
+                <AllEmployeeVisaPages />,
+                <InProcessPage />
+            ]}
+        />
+
     );
 }
